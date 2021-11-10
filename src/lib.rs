07f6080c60522034691cfc6d4637935a7094a77a
calacuda <eowest@gmail.com>
2021-11-10 00:54:01 -0500

    removed form token

--- src/lib.rs
+++ src/lib.rs
@@ -23,14 +23,6 @@ use std::collections::HashMap;
 
 // the deffinitions of all standard library functions go here.
 
-fn make_bool(input: &str) -> bool {
-    match input {
-        "t" => true,
-        "nill" => false,
-        _ => panic!("external library function returned a not boolean but clamed it as a bool."),
-    }
-}
-
 #[no_mangle]
 pub fn not<'a>(truth: &Vec<Token>) -> Result<Option<Token>, &'a str> {
     /*
@@ -46,16 +38,24 @@ pub fn not<'a>(truth: &Vec<Token>) -> Result<Option<Token>, &'a str> {
 
 #[no_mangle]
 pub fn write_line<'a>(lines: &Vec<Token>) -> Result<Option<Token>, &'a str> {
-    let _ = write(lines);
+    let _ = printf(lines);
     println!();
     return Ok(None);
 }
 
 #[no_mangle]
 pub fn write<'a>(lines: &Vec<Token>) -> Result<Option<Token>, &'a str> {
-    // println!("write args: {:?}", lines);
-    let mut loc_lines = lines.clone();
+    printf(lines);
+    return Ok(None);
+}
+
+fn printf(tokens: &Vec<Token>) {
+    // println!("write args: {:?}", tokens);
+    let mut loc_lines = tokens.clone();
+    // println!("loc_lines: {:?} ", loc_lines);
     let last_i = loc_lines.len();
+    // println!("{} ", last_i);
+
     for i in 0..last_i {
         let line = loc_lines.pop().unwrap();
         print_line(line);
@@ -63,16 +63,14 @@ pub fn write<'a>(lines: &Vec<Token>) -> Result<Option<Token>, &'a str> {
             print!(" ");
         }
     }
-    return Ok(None);
 }
 
-#[no_mangle]
 fn print_line(line: Token) {
     match line {
         Token::Symbol(output) => print!("{}", output),
         Token::Number(output) => print!("{}", output),
         Token::Str(output) => print!("{}", output),
-        Token::Form(form) => print_form(*form),
+        // Token::Form(form) => print_form(*form),
         Token::Bool(truth) => print!(
             "{}",
             match truth {
@@ -84,14 +82,14 @@ fn print_line(line: Token) {
     }
 }
 
-#[no_mangle]
-fn print_form(form: Vec<Token>) {
-    let mut loc_form = form.clone();
-    loc_form.reverse();
-    print!("`(");
-    let _ = write(&loc_form);
-    print!(")");
-}
+// #[no_mangle]
+// fn print_form(form: Vec<Token>) {
+//     let mut loc_form = form.clone();
+//     loc_form.reverse();
+//     print!("`(");
+//     let _ = write(&loc_form);
+//     print!(")");
+// }
 
 #[no_mangle]
 pub fn terpri<'a>(_things: &Vec<Token>) -> Result<Option<Token>, &'a str> {
@@ -146,7 +144,7 @@ pub fn get_funcs<'a>() -> HashMap<
     (
         Nargs,
         String,
-        //&'a (dyn Fn(&Vec<Token>) -> Result<Option<Token>, &'a str>),
+        // &'a (dyn Fn(&Vec<Token>) -> Result<Option<Token>, &'a str>),
     ),
 > {
     let mut std_funcs: HashMap<
@@ -166,15 +164,13 @@ pub fn get_funcs<'a>() -> HashMap<
     // std_funcs.push(("not", Nargs::Num(1), &not));
     // std_funcs.push(("!", Nargs::Num(1), &not));
 
-    // std_funcs.insert("write".to_string(), (Nargs::INF, "write".to_string()));
-    // std_funcs.insert("print".to_string(), (Nargs::INF, "write".to_string()));
-    // std_funcs.insert(
-    //     "write-line".to_string(),
-    //     (Nargs::INF, "write_line".to_string()),
-    // );
-    // std_funcs.insert("terpri".to_string(), (Nargs::Num(0), "terpri".to_string()));
-    // std_funcs.insert("not".to_string(), (Nargs::Num(1), "not".to_string()));
-    // std_funcs.insert("!".to_string(), (Nargs::Num(1), "not".to_string()));
+    // std_funcs.insert("write".to_string(), (Nargs::INF, &write));
+    // std_funcs.insert("print".to_string(), (Nargs::INF, &write));
+    // std_funcs.insert("write-line".to_string(), (Nargs::INF, &write_line));
+    // std_funcs.insert("writeln".to_string(), (Nargs::INF, &write_line));
+    // std_funcs.insert("terpri".to_string(), (Nargs::Num(0), &terpri));
+    // std_funcs.insert("not".to_string(), (Nargs::Num(1), &not));
+    // std_funcs.insert("!".to_string(), (Nargs::Num(1), &not));
 
     add_fn(&mut std_funcs, "write", Nargs::INF, "write");
     add_fn(&mut std_funcs, "print", Nargs::INF, "write");
